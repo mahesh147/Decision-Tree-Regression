6ee54362af36d8281630d5213539e1d26b83d721
mahesh147 <maheshchandran147@gmail.com>
2018-01-14 15:11:11 +0530

    Final commit

---
+++ decision_tree_regression.py
@@ -0,0 +1,34 @@
+# Regression Template
+
+# Multiple Linear Regression
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the datasets
+
+datasets = pd.read_csv('Position_Salaries.csv')
+X = datasets.iloc[:, 1:2].values
+Y = datasets.iloc[:, 2].values
+
+# Fitting the Regression model to the dataset
+
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state = 0)
+regressor.fit(X,Y)
+
+# Predicting a new result with the Decision Tree Regression
+
+Y_Pred = regressor.predict(6.5)
+
+# Visualising the Decision Tree Regression results in higher resolution and smoother curve
+X_Grid = np.arange(min(X), max(X), 0.01)
+X_Grid = X_Grid.reshape((len(X_Grid), 1))
+plt.scatter(X,Y, color = 'red')
+plt.plot(X_Grid, regressor.predict(X_Grid), color = 'blue')
+plt.title('Decision Tree Regression Results')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
